1b3f4aa7d91521cc8436eaa945db24da4a07aec6
Selvam6994 <selvamyoor@gmail.com>
2023-03-09 16:42:03 +0530

    front end otp verification done

--- src/Forgotpassword.jsx
+++ src/Forgotpassword.jsx
@@ -7,7 +7,7 @@ import { json } from "react-router-dom";
 function Forgotpassword() {
   const [message, setMessage] = useState("");
   const [otpTextfield, setOtpSetField] = useState(false);
-  const formik = useFormik({
+  const formik_mail = useFormik({
     initialValues: {
       email: "",
     },
@@ -29,38 +29,68 @@ function Forgotpassword() {
       }
     },
   });
+
+  const formik_otp = useFormik({
+    initialValues: {
+      otp: "",
+    },
+    onSubmit: async (values) => {
+      const get_otp = await fetch(
+        "http://localhost:4000/mobileData/otp",
+        {
+          method: "POST",
+          headers: { "Content-type": "application/json" },
+          body: JSON.stringify(values),
+        }
+      );
+      if (get_otp.status == 200) {
+        const result = await get_otp.json();
+        setMessage("otp verified");
+        setOtpSetField(true);
+       
+      } else {
+        setMessage("Invalid credentials");
+      }
+    },
+  });
   return (
-    <form onSubmit={formik.handleSubmit} className="form">
+   <div>
       <h2>Reset password</h2>
-      <TextField
+      
+      {
+        (otpTextfield == false ? (
+          <form onSubmit={formik_mail.handleSubmit} className="form">
+          <TextField
         name="email"
-        onChange={formik.handleChange}
-        value={formik.values.email}
+        onChange={formik_mail.handleChange}
+        value={formik_mail.values.email}
         id="standard-basic"
         label="Valid email"
         variant="standard"
       />
-      {
-        (otpTextfield == true ? (
+      <Button type="submit" variant="outlined">
+        Generate OTP
+      </Button>
+      </form>
+        ) : (
+          <form onSubmit={formik_otp.handleSubmit} className="form">
           <TextField
             name="otp"
-            // onChange={formik.handleChange}
-            // value={formik.values.email}
+            onChange={formik_otp.handleChange}
+            value={formik_otp.values.otp}
             id="standard-basic"
             label="OTP"
             variant="standard"
           />
-        ) : (
-          ""
+          <Button type="submit" variant="outlined">
+        Submit
+      </Button>
+      </form>
         ))
       }
-      <Button type="submit" variant="outlined">
-        {
-          otpTextfield==false?"Generate OTP" : "Submit"
-        }
-      </Button>
+      
       <h2>{message}</h2>
-    </form>
+      </div>
   );
 }
 
